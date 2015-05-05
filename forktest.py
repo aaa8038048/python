6b7dec59a79e0364cf79aae550b570e6a916d17d
aaa8038048 <369458311@qq.com>
2015-05-05 11:25:39 +0800

    Delete forktest.py

--- forktest.py
+++
@@ -1,9 +0,0 @@
-import os
-def myfork():
-	pid=os.fork()
-	if pid==0:
-		print "this is child %d-----%d" % (pid,os.getpid())
-	else:
-		print "this is parent %d-----%d" % (pid,os.getpid())
-if __name__=='__main__':
-	myfork()
